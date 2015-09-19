a1a9cfe879ded0f658e3a17153c1c7f9edff187b
REDJEEMac <santosh@REDJEEMacs-Mac-mini.local>
2015-09-19 12:36:46 +0530

    Initial Commit

---
+++ Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sample
+//
+//  Created by REDJEEMac on 19/09/15.
+//  Copyright (c) 2015 REDJEEMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sample
+//
+//  Created by REDJEEMac on 19/09/15.
+//  Copyright (c) 2015 REDJEEMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
